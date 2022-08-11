a12a789aa16dca9367f9ee1268a3c10c23694a4e
Elane Garcia <elanegarcia@ciandt.com>
2022-08-11 16:43:19 -0300

    Task 0

---
+++ SongApi/src/test/java/com/example/SongApi/SongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SongApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
