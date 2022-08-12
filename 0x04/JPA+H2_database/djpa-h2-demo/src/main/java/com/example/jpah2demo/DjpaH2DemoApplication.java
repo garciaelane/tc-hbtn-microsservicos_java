73e6b1793d3e120b7118ad1454d46ed378326427
Elane Garcia <elanegarcia@ciandt.com>
2022-08-12 01:20:32 -0300

    Task 0

---
+++ 0x04/JPA+H2_database/djpa-h2-demo/src/main/java/com/example/jpah2demo/DjpaH2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpah2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjpaH2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjpaH2DemoApplication.class, args);
+	}
+
+}
